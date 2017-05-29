0f1f56ed2182ed498bb37cbed0258a825ba8bef0
SangHyoun Yun <hainco@naver.com>
2017-05-29 14:02:08 +0900

    Change to ES6 Style

--- GServer/routes/index.js
+++ GServer/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const models = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
