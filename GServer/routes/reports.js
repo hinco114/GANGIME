fe4a8f6359191176bdbe0772eb011a8a69e2ecb9
DAhYE24 <qismine55@naver.com>
2017-05-31 14:23:54 +0900

    inactivation token

--- GServer/routes/reports.js
+++ GServer/routes/reports.js
@@ -5,35 +5,35 @@ const token_verify = require('../token_verify');
 const r_models = require('../models/').REPORTS_TB;
 const e_models = require('../models/').ERRANDS_TB;
 
-router.route('/reports').get(reportUser);
+// router.route('/reports').get(reportUser);
 
-/* 1. 신고하기 */
-async function reportUser(req, res, next){
-    let errandIdx = req.body.errandIdx;
-    let reportContent = req.body.reportContent;
-    let token = req.headers['token']; // 토큰
-    let decodedToken = token_verify(token);
-
-    if (!errandIdx || !reportContent) throw new Error("내용 누락");
-    // 유저 인덱스 존재 여부 체크
-    try{
-        let result = await createReport(decodedToken.userIdx, errandIdx, reportContent);
-        resSucc(res, result);
-    } catch(err){
-        next(err);
-    }
-}
-
-/* 1_1 DB에 신고 등록하기 */
-const createReport = (userIdx, errandIdx, reportContent) => {
-    return new Promise((resolve, reject) => {
-        const result = r_models.create({userIdx: userIdx, errandIdx: errandIdx, reportContent: reportContent});
-            // n_models.findOne({
-            // where: {noticeIdx: noticeIdx},
-            // attributes: ['noticeTitle', 'noticeContent', 'createdAt']});
-        if(result) resolve(result);
-        else reject('error');
-    });
-}
+// /* 1. 신고하기 */
+// async function reportUser(req, res, next){
+//     let errandIdx = req.body.errandIdx;
+//     let reportContent = req.body.reportContent;
+//     let token = req.headers['token']; // 토큰
+//     let decodedToken = token_verify(token);
+//
+//     if (!errandIdx || !reportContent) throw new Error("내용 누락");
+//     // 유저 인덱스 존재 여부 체크
+//     try{
+//         let result = await createReport(decodedToken.userIdx, errandIdx, reportContent);
+//         resSucc(res, result);
+//     } catch(err){
+//         next(err);
+//     }
+// }
+//
+// /* 1_1 DB에 신고 등록하기 */
+// const createReport = (userIdx, errandIdx, reportContent) => {
+//     return new Promise((resolve, reject) => {
+//         const result = r_models.create({userIdx: userIdx, errandIdx: errandIdx, reportContent: reportContent});
+//             // n_models.findOne({
+//             // where: {noticeIdx: noticeIdx},
+//             // attributes: ['noticeTitle', 'noticeContent', 'createdAt']});
+//         if(result) resolve(result);
+//         else reject('error');
+//     });
+// }
 
 module.exports = router;
--- GServer/token_verify.js
+++ GServer/token_verify.js
@@ -1,15 +1,16 @@
 /* 토큰 복호화 */
 const jwt = require('jsonwebtoken');
 const secretKey = 'ggmtest'; // SECRET KEY
-
-const tokenVerifier = (token) => {
-    if (token) {
-        console.log('token :', token);
-        jwt.verify(token, secretKey, (err, decoded) => {
-            // if (decoded) req.user = decoded;
-            if (decoded) return decoded;
-        });
-    }else throw new Error("토큰 누락");
-}
-
-module.exports = tokenVerifier();
+//
+// const tokenVerifier = (token) => {
+//     if (token) {
+//         console.log('token :', token);
+//         jwt.verify(token, secretKey, (err, decoded) => {
+//             // if (decoded) req.user = decoded;
+//             if (decoded) return decoded;
+//         });
+//     }else
+//         throw new Error("토큰 누락");
+// }
+//
+// module.exports = tokenVerifier();
