0464be2b8d6b0f087cede2a579f55b6de0bf4043
SangHyoun Yun <hainco@naver.com>
2017-06-01 10:22:20 +0900

    Check headers not have token

--- GServer/routes/gangime.js
+++ GServer/routes/gangime.js
@@ -18,10 +18,13 @@ const resSucc = (res, data) => {
     res.json(resultModel);
 };
 
-async function tokenVerify(token) {
+async function tokenVerify(headers) {
     return new Promise((resolve, reject) => {
         try {
-            const decoded = jwt.verify(token, jwtConfig.SECRET_KEY);
+            if (!headers.token) {
+                throw new Error("Token not in Headers");
+            }
+            const decoded = jwt.verify(headers.token, jwtConfig.SECRET_KEY);
             resolve({userIdx: decoded.userIdx});
         } catch (err) {
             reject(err);
