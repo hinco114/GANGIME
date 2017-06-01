262351957ee193226536927e8fe1a9e59b636b78
DAhYE24 <qismine55@naver.com>
2017-06-01 14:24:06 +0900

    compelte showErrandDetails

--- GServer/routes/errands.js
+++ GServer/routes/errands.js
@@ -4,14 +4,13 @@ const resSucc = require('./gangime').resSucc;
 const tokenVerify = require('./gangime').tokenVerify;
 const e_models = require('../models/').ERRANDS_TB;
 
-router.route('/errand').post(registerErrand);
+router.route('/errands').post(registerErrand);
 router.route('/errands/:errandsIdx')
     .get(showErrandDetail);
 
 /* 1. 심부름 등록하기 */
 async function registerErrand(req, res, next) {
     let body = req.body;
-    // let decodedToken = tokenVerify(req.headers);
     const userIdx = await tokenVerify(req.headers);
 
     //TODO : DH_내용 누락되는 경우 체크 필요성?
@@ -35,12 +34,9 @@ async function showErrandDetail(req, res, next) {
 /* 1_1 DB에 심부름 등록 */
 const createErrand = (body, userIdx) => {
     return new Promise((resolve, reject) => {
-        // 입력해야하는 부분 추가한 후 DB에서 create
-        // TODO: DH_token, errandChatId => MongoDB 연결
         let inputData = body;
-        console.log(inputData);
         inputData.requesterIdx = userIdx;
-        inputData.errandChatId = 'chatId'; // Mongo
+        inputData.errandChatId = 'chatId'; // TODO: errandChatId => MongoDB 연결
         inputData.errandStatus = '입금대기';
 
         const result = e_models.create(inputData);
@@ -54,31 +50,27 @@ const createErrand = (body, userIdx) => {
 }
 
 /* 2_1 해당 심부름의 내역 가져오기 */
-function getErrandDetail(errandIdx) {
-    return new Promise((resolve, reject) => {
+const getErrandDetail = (errandIdx) => {
+    return new Promise(async (resolve, reject) => {
         let inputData = ['requesterIdx', 'errandTitle', 'errandContent',
             'startStationIdx', 'arrivalStationIdx', 'stationDistance',
             'deadlineDt', 'itemPrice', 'errandPrice', 'errandStatus'];
+        try{
+            let statusChk = await e_models.findOne({
+                where: {errandIdx: errandIdx}, attributes: ['errandStatus']
+            });
 
-        // TODO: async-await로 수정
-        let statusChk = e_models.findOne({
-            where: {errandIdx: errandIdx}, attributes: ['cancelReason']
-        });
+            if (statusChk.dataValues.errandStatus == '취소완료') { // status = "취소완료"일 때만 cancelReason 컬럼 반환
+                inputData.push('cancelReason');
+            }
 
-        if (statusChk == '취소완료') { // status = "취소완료"일 때만 cancelReason 컬럼 반환
-            inputData.push('cancelReason');
-        }
-
-        const result = e_models.findOne({
-            where: {errandIdx: errandIdx},
-            attributes: inputData
-        });
-
-        if (result) {
+            const result = e_models.findOne({
+                where: {errandIdx: errandIdx},
+                attributes: inputData
+            });
             resolve(result);
-        }
-        else {
-            reject('error');
+        }catch(err){
+            reject(err)
         }
     });
 }
