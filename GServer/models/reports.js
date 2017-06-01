62fb86c4547ba546df1311c1462811e320368003
DAhYE24 <qismine55@naver.com>
2017-06-01 17:06:44 +0900

    complete reports api

--- GServer/models/reports.js
+++ GServer/models/reports.js
@@ -1,8 +1,7 @@
 /**
  * REPORTS_TB 신고 테이블
  */
-const u_models = require('../models/').USERS_TB;
-const e_models = require('../models/').ERRANDS_TB;
+const models = require('../models/');
 
 module.exports = (sequelize, DataTypes) => {
     const reports_tb = sequelize.define('REPORTS_TB', {
@@ -12,9 +11,9 @@ module.exports = (sequelize, DataTypes) => {
         reportContent: {type: DataTypes.STRING, allowNull: false}
     }, {tableName: 'REPORTS_TB', comment: '신고 테이블',
         classMethods:{
-            associate: function(u_models, e_models){
-                u_models.USERS_TB.hasMany(reports_tb, {foreignKey : 'userIdx'});
-                // e_models.ERRANDS_TB.hasMany(reports_tb, {foreignKey : 'errandIdx'});
+            associate: function(models){
+                models.USERS_TB.hasMany(reports_tb, {foreignKey : 'userIdx'});
+                models.ERRANDS_TB.hasMany(reports_tb, {foreignKey : 'errandIdx'});
             }
         }
     });
