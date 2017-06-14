f76805da0663cef7beb2791c596e043e2fae6d8e
SangHyoun Yun <hainco@naver.com>
2017-06-14 17:27:58 +0900

    add Line info to STATION_TB

--- GServer/models/stations.js
+++ GServer/models/stations.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const stations_tb = sequelize.define('STATIONS_TB', {
         stationIdx: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
         stationName: {type: DataTypes.STRING(20), allowNull: false, unique: true},
+        stationLines: {type: DataTypes.STRING, allowNull: false},
         stationLocation: {type: DataTypes.GEOMETRY('POINT'), allowNull: false}
     }, {
         timestamps: false,
