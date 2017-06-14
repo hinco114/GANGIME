129700fcdce5d4d129efb8b70e62e11b584ca3c9
SangHyoun Yun <hainco@naver.com>
2017-06-14 10:41:08 +0900

    send Error logs to slack monitoring channel

--- GServer/app.js
+++ GServer/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 // const favicon = require('serve-favicon');
 const logger = require('morgan');
+const Request = require('request');
 // const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
@@ -23,7 +24,7 @@ const app = express();
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
@@ -37,20 +38,42 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    // res.locals.message = err.message;
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({msg: err.message, data: null});
+    // render the error page
+    res.status(err.status || 500);
+    res.json({msg: err.message, data: null});
+    const slackConfig = require('./config/config.json')['slack'];
+    Request
+        .post(slackConfig.url)
+        .form({
+            payload: JSON.stringify({
+
+                'channel': slackConfig.channel,
+                'username': slackConfig.username,
+                'icon_emoji': slackConfig.icon_emoji,
+                'attachments': [
+                    {
+                        'color': 'danger',
+                        'pretext': 'Error has benn occuerd',
+                        'title': req.url,
+                        'text': err.message,
+                        'footer': 'From_AWS',
+                        'footer_icon': 'https://platform.slack-edge.com/img/default_application_icon.png',
+                        'ts': new Date().getTime() / 1000
+                    }
+                ]
+            })
+        });
 });
 
 module.exports = app;
